302865147c607f7497888eee73f9c3d159876b22
Ale Figueroa <yknx.4.b@gmail.com>
2017-08-21 02:08:51 -0500

    timeout callback

--- scripts/waitForResponseSupport.js
+++ scripts/waitForResponseSupport.js
@@ -4,17 +4,21 @@ const timeoutMap = {};
 const waitingKey = userId => `${userId}waitingForResponse`;
 
 module.exports = function useNaturals(robot) {
-  robot.on("expectResponse", (userId, callback, ttl = 10000) => {
-    robot.brain.set(waitingKey(userId), true);
-    cbMap[userId] = callback;
-    timeoutMap[userId] = setTimeout(() => {
-      if (robot.brain.get(waitingKey(userId)) === true) {
-        robot.brain.set(waitingKey(userId), false);
-        robot.messageRoom(userId, "nevermind");
-        delete cbMap[userId];
-      }
-    }, ttl);
-  });
+  robot.on(
+    "expectResponse",
+    (userId, callback, { ttl = 10000, timeout = () => {} }) => {
+      robot.brain.set(waitingKey(userId), true);
+      cbMap[userId] = callback;
+      timeoutMap[userId] = setTimeout(() => {
+        if (robot.brain.get(waitingKey(userId)) === true) {
+          robot.brain.set(waitingKey(userId), false);
+          robot.messageRoom(userId, "nevermind");
+          timeout(robot, userId);
+          delete cbMap[userId];
+        }
+      }, ttl);
+    }
+  );
   return robot.receiveMiddleware((context, next, done) => {
     const { response } = context;
     const { message } = response;
