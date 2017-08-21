a20833b2671e1ee153198b415316086233128a98
Ale Figueroa <alejandro@progressly.com>
2017-08-21 13:08:54 -0500

    Only send one sorry message when not understanding

--- scripts/random.js
+++ scripts/random.js
@@ -3,7 +3,7 @@ const request = require('request-promise');
 
 module.exports = robot => {
   robot.catchAll(async res => {
-    res.reply(`I didn't understand...`);
+    res.send("Sorry, I couldn't understand you :(");
     const gifSearch = await request(
       `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(
         res.message.text.replace(robot.name, '').trim(),
@@ -11,7 +11,6 @@ module.exports = robot => {
       { json: true },
     );
     if (robot.adapterName === 'telegram') {
-      res.send("Sorry, I couldn't understand you :(");
       res.send('But here is a gif ~');
       const images = gifSearch.data[0].images;
       const video = images.original.url;
