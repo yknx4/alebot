752354f545b4c9138968f4c24adff8ea7c82a424
Ale Figueroa <yknx.4.b@gmail.com>
2017-08-22 00:06:30 -0500

    Add fallback regex to shower matchers

--- src/natural_matchers/SetShowerMatcher.js
+++ src/natural_matchers/SetShowerMatcher.js
@@ -1,19 +1,24 @@
-const NaturalMatcher = require('./NaturalMatcher');
-const { TOOK_SHOWER } = require('../classifications');
+const NaturalMatcher = require("./NaturalMatcher");
+const { TOOK_SHOWER } = require("../classifications");
 
-const SHOWERED_ANSWERS = ['Registered!', 'Gotcha!', 'You needed some water already...'];
+const SHOWERED_ANSWERS = [
+  "Registered!",
+  "Gotcha!",
+  "You needed some water already..."
+];
 
 class SetShowerMatcher extends NaturalMatcher {
   async execute() {
     const { res, robot } = this;
     res.reply(res.random(SHOWERED_ANSWERS));
-    robot.brain.set('lastShower', new Date().toISOString());
+    robot.brain.set("lastShower", new Date().toISOString());
   }
 }
 
-SetShowerMatcher.keywords = ['shower'];
+SetShowerMatcher.keywords = ["shower", "bath"];
 SetShowerMatcher.regex = /.*[^\?]$/i;
-SetShowerMatcher.description = 'Update last time you took a shower.';
+SetShowerMatcher.fallbackRegex = /took(.*)shower?[^\?]$/i;
+SetShowerMatcher.description = "Update last time you took a shower.";
 
 NaturalMatcher.register(SetShowerMatcher, TOOK_SHOWER);
 
