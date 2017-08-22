8277bde7232e8625c866f608da391b5b01508c5f
Ale Figueroa <alejandro@progressly.com>
2017-08-22 10:35:47 -0500

    Extract more helpers for GithubPullRequestsMatcher

--- src/natural_matchers/GithubPullRequestsMatcher.helpers.js
+++ src/natural_matchers/GithubPullRequestsMatcher.helpers.js
@@ -1,3 +1,12 @@
+const { isEmpty } = require('lodash');
+
 exports.onlyMineRegex = /\s(my|mine)\s/i;
-exports.filterByUser = (user, onlyMine) =>
-  onlyMine ? pr => pr.user.login === user : () => true;
+exports.filterByUser = (user, onlyMine) => (onlyMine ? pr => pr.user.login === user : () => true);
+exports.addReviewerData = pr => {
+  const { requested_reviewers } = pr;
+  pr.hasReviewers = !isEmpty(requested_reviewers);
+  pr.reviewers = requested_reviewers.map(r => `_${r.login}_`).join(', ');
+  return pr;
+};
+exports.pullRequestsTemplate = `These are {{noun}} open *Pull Requests* on {{owner}}/{{repo}}
+{{#prs}}\t- {{#notme}}_{{user.login}}_{{/notme}} *#{{number}}* [{{{title}}}]({{{url}}}).{{#hasReviewers}} Reviewers: {{reviewers}}{{/hasReviewers}}\n{{/prs}}`;
--- src/natural_matchers/GithubPullRequestsMatcher.js
+++ src/natural_matchers/GithubPullRequestsMatcher.js
@@ -1,11 +1,13 @@
-const Mustache = require("mustache");
-const NaturalMatcher = require("./NaturalMatcher");
-const { GITHUB_PR_PLURAL } = require("../classifications");
-const { github, loadAll } = require("../helpers/github.helpers");
+const Mustache = require('mustache');
+const NaturalMatcher = require('./NaturalMatcher');
+const { GITHUB_PR_PLURAL } = require('../classifications');
+const { github, loadAll } = require('../helpers/github.helpers');
 const {
   onlyMineRegex,
-  filterByUser
-} = require("./GithubPullRequestsMatcher.helpers");
+  filterByUser,
+  addReviewerData,
+  pullRequestsTemplate,
+} = require('./GithubPullRequestsMatcher.helpers');
 
 const user = process.env.GITHUB_USER;
 
@@ -13,7 +15,7 @@ class GithubPullRequestsMatcher extends NaturalMatcher {
   get fullRepo() {
     const { fallback, matches } = this;
     const repoMatchPosition = fallback ? 4 : 2;
-    return matches[repoMatchPosition].split("/");
+    return matches[repoMatchPosition].split('/');
   }
 
   get repoName() {
@@ -35,55 +37,34 @@ class GithubPullRequestsMatcher extends NaturalMatcher {
   }
 
   async execute() {
-    const {
-      res,
-      repoName: repo,
-      repoOwner: owner,
-      hasRepoName,
-      onlyMine,
-      legacy,
-      text
-    } = this;
+    const { res, repoName: repo, repoOwner: owner, hasRepoName, onlyMine, legacy, text } = this;
     if (!hasRepoName) return null;
     if (legacy) {
       classifier.addDocument(text, GITHUB_PR_PLURAL);
     }
 
-    res.send(
-      `Loading ${onlyMine ? "your " : ""}*Pull Requests* for ${owner}/${repo}`
-    );
+    res.send(`Loading ${onlyMine ? 'your ' : ''}*Pull Requests* for ${owner}/${repo}`);
 
     try {
       const prs = await loadAll(github.pullRequests.getAll, {
         owner,
         per_page: 100,
-        repo
+        repo,
       });
 
       const view = {
-        noun: onlyMine ? "your" : "the",
+        noun: onlyMine ? 'your' : 'the',
         repo,
         owner,
         notme: onlyMine === false,
-        prs: prs.filter(filterByUser(user, onlyMine)).map(p => {
-          p.hasReviewers = p.requested_reviewers.length > 0;
-          p.reviewers = p.requested_reviewers
-            .map(r => `_${r.login}_`)
-            .join(", ");
-          return p;
-        })
+        prs: prs.filter(filterByUser(user, onlyMine)).map(addReviewerData),
       };
 
       if (view.prs.length === 0) {
-        return res.send(
-          `There are no open *Pull Requests* on ${owner}/${repo}`
-        );
+        return res.send(`There are no open *Pull Requests* on ${owner}/${repo}`);
       }
 
-      const prTemplate = `These are {{noun}} open *Pull Requests* on {{owner}}/{{repo}}
-{{#prs}}\t- {{#notme}}_{{user.login}}_{{/notme}} *#{{number}}* [{{{title}}}]({{{url}}}).{{#hasReviewers}} Reviewers: {{reviewers}}{{/hasReviewers}}\n{{/prs}}`;
-
-      return res.send(Mustache.render(prTemplate, view));
+      return res.send(Mustache.render(pullRequestsTemplate, view));
     } catch (error) {
       logger.error(error);
       return res.send(`Cannot get pull requests for ${owner}/${repo}`);
@@ -91,12 +72,7 @@ class GithubPullRequestsMatcher extends NaturalMatcher {
   }
 }
 
-GithubPullRequestsMatcher.keywords = [
-  "pull requests",
-  "pull request",
-  "prs",
-  "pr"
-];
+GithubPullRequestsMatcher.keywords = ['pull requests', 'pull request', 'prs', 'pr'];
 
 GithubPullRequestsMatcher.regex = /\s(for|in) (\w*\/\w*|\w*)\s*/i;
 GithubPullRequestsMatcher.fallbackRegex = /(my)? ?(prs?|pull requests?) (for|in) (\w*\/\w*|\w*)/i;
