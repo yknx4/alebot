9ef47e2fc36622bbad38052e11c4edeffcc2a6ef
Ale Figueroa <yknx.4.b@gmail.com>
2017-08-23 09:26:09 -0500

    Better logging

--- src/logger.js
+++ src/logger.js
@@ -1,29 +1,41 @@
-const bunyan = require('bunyan');
+const bunyan = require("bunyan");
+const bunyanDebugStream = require("bunyan-debug-stream");
+const path = require("path");
+
+const config = {
+  name: "AleBot",
+  src: process.env.NODE_ENV !== "production",
+  streams: [
+    {
+      level: "error",
+      path: "./error.log"
+    },
+    {
+      level: "trace",
+      path: "./trace.log"
+    }
+  ]
+};
+
+if (process.env.NODE_ENV !== "production") {
+  config.streams.push({
+    level: "info",
+    type: "raw",
+    stream: bunyanDebugStream({
+      basepath: path.join(__dirname, "../"), // this should be the root folder of your project.
+      forceColor: true
+    })
+  });
+  config.serializers = bunyanDebugStream.serializers;
+}
 
 function setupLogger() {
-  global.logger = bunyan.createLogger({
-    name: 'AleBot',
-    src: process.env.NODE_ENV !== 'production',
-    streams: [
-      {
-        level: 'trace',
-        stream: process.stdout,
-      },
-      {
-        level: 'error',
-        path: './error.log',
-      },
-      {
-        level: 'trace',
-        path: './trace.log',
-      },
-    ],
-  });
+  global.logger = bunyan.createLogger(config);
 }
 if (global.logger == null) {
   setupLogger();
-  global.logger.info('logger initialized');
-  process.on('unhandledRejection', error => {
+  global.logger.info("logger initialized");
+  process.on("unhandledRejection", error => {
     global.logger.error(error);
   });
 }
